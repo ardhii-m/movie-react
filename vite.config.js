81e0cc7a924d64ccb795cb08aeaa50a5dd384d66
ardhii-m <ardhiim008@gmail.com>
2025-02-28 13:43:26 +0700

    Adding PWA

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,52 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      includeAssets: [
+          "**/*",
+      ],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Movie Catalogue',
+        short_name: 'Movies',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#2c3e50',
+        theme_color: '#3498db',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'  
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+        type: 'module',
+        navigateFallback: 'index.html',
+      },
+    }),
   ],
 })
